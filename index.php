b3a6e2409a04fe5a61b528ce16598897bbf4ec04
tammykoh <45681363+tammykoh@users.noreply.github.com>
2018-12-07 22:34:33 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Zero Energy Garden</title>
+        <link rel="stylesheet" type="text/css" href="includes/style.css" />
+    </head>
+    
+    <body>
+        <!-- Header -->
+            <?php include ('includes/header.php'); ?>
+        
+        <div class="wrapper1">
+            <table class="display">
+                <tr>
+                    <td width="15%"><a href="Garden1.php" class="box-text">Garden 1</a></td>
+                    <td width="15%"><a href="Garden2.php" class="box-text">Garden 2</a></td>
+                    <td width="15%"><a href="Garden3.php" class="box-text">Garden 3</a></td>
+                    <td width="55%"></td>
+                </tr><br>
+            </table>
+        </div>
+        
+        <!-- Footer -->
+            <?php include ('includes/footer.php'); ?>
+        
+    </body>
+</html>
